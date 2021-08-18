125f424a41dad92957e16adb74c28bbefc2cdf3a
Hartman <52998392+tebenkov2222@users.noreply.github.com>
2021-08-18 19:04:11 +0400

    fix

--- Assets/Scripts/Animation/AtackSTB.cs
+++ Assets/Scripts/Animation/AtackSTB.cs
@@ -5,11 +5,11 @@ public class AtackSTB : StateMachineBehaviour
     public int MaxIndAtack;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetInteger("AtackInd", Random.Range(0,MaxIndAtack));
+        animator.SetInteger("AttackInd", Random.Range(0,MaxIndAtack));
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("Atack", false);
+        animator.SetBool("Attack", false);
     }
 }
